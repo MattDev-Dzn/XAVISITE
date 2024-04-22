c274a2c22c1c1fc1d85b3dd2794fe1d89be70b81
MattDEV <78957469+MattDev-Dzn@users.noreply.github.com>
2024-04-22 13:50:40 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// Liste des chemins d'accès des images d'arrière-plan
+var images = ["IMG/BG3.jpg", "IMG/BG5.jpg", "IMG/BG7.jpg", "IMG/BG9.jpg"];
+
+// Fonction pour changer l'image d'arrière-plan
+function changeBackground() {
+    // Générer un index aléatoire pour choisir une image d'arrière-plan
+    var randomIndex = Math.floor(Math.random() * images.length);
+    
+    // Changer l'image d'arrière-plan en fonction de l'index aléatoire
+    document.body.style.backgroundImage = "url('" + images[randomIndex] + "')";
+    
+}
+
+// Appeler la fonction pour changer l'arrière-plan lors du chargement de la page
+window.onload = changeBackground;
